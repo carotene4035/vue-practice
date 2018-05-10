73952f5278bb8fd683a82cf01275e1e8ad60b35b
carotene512 <karoten512@gmail.com>
2018-05-10 09:06:50 +0900

    componentにdataをもたせた。

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-// コンポーネントの定義
+/** コンポーネントの定義 */
 var mycomponent = {
-  template: '<div>hello</div>'
+  template: '<div>hello {{message}}<button v-on:click="count++">You clicked me {{ count }} times.</button></div>',
+  data: function() {
+    return {
+      message: 'taichi',
+      count: 0
+    }
+  }
 };
 
-// コンポーネントの登録
+/** コンポーネントの登録 */
 Vue.component('my-component', mycomponent);
+
 
 /** 今後生成されるすべてのvueインスタンスで使えるようになる */
 var app = new Vue({
